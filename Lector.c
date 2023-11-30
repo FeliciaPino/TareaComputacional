fb580a25ca9aeb1ef626802467edd82bc7bc2728
DoubleYou <146099765+Rhussu@users.noreply.github.com>
2023-11-30 15:58:47 -0300

    Nueva version de Lector.c

--- Lector.c
+++ Lector.c
@@ -2,25 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-char** lector(char* hola, char** lectura) {
+char** lector(const char* hola, char** lectura) {
     char frase[50];
     strcpy(frase, hola);
-    char* token = strtok(frase, " ");
-    int aux = 0;
     for (int i = 0; i < 6; i++) {
         lectura[i] = NULL;
     }
+    char* token = strtok(frase, " \"");
+    int aux = 0;
     while (token != NULL && aux < 6) {
-        lectura[aux] = strdup(token);
-        aux++;
-        token = strtok(NULL, " ");
+        if (strcmp(token, " ") != 0 && strcmp(token, "") != 0) {
+            lectura[aux] = strdup(token);
+            aux++;
+        }
+        token = strtok(NULL, " \"");
     }
     return lectura;
 }
 
 int main() {
     char* lectura[6];
-    lector("Ongolmo 325 paicavi 309 san_martin 567", lectura);
+    lector("\"Ongolmo 325\" \"paicavi 309\" \"san_martin 567\"", lectura);
     for (int i = 0; i < 6; i++) {
         if (lectura[i] != NULL) {
             printf("%s\n", lectura[i]);
